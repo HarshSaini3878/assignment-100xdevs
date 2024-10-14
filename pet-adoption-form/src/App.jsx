92f9b692fff6b2256ede32464335b83b5d9f2e98
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-10-14 18:15:10 +0530

    1st

---
+++ pet-adoption-form/src/App.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+ const [data,setData]=useState([]);
+  return (
+    <ChakraProvider>
+   
+    </ChakraProvider>
+  )
+}
+function Layout(){
+  return (
+    <div>
+      
+    </div>
+  )
+}
